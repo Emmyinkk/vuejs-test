d2ad199a64022984c38cf90dabbfbfae3776086f
Emmanuel <olaoluwafagbemi@gmail.com>
2022-05-31 02:02:57 +0100

    test commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="test">
+      <h3>Vuex</h3>
+      <h4> {{ $store.state.counter }}</h4>
+      <button @click="addOne">Add 10</button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+  },
+  methods: {
+    addOne() {
+      this.$store.commit('increment', { value: 10});
+    }
   }
 }
 </script>
@@ -22,5 +29,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.test {
+  padding: 1em 5em;
+  background: white;
+  box-shadow: 1px 1px 3px 4px #ccc;
+  width: 50%;
+  margin: 0 auto;
+}
+h3 {
+  margin-bottom: 2em;
 }
 </style>
